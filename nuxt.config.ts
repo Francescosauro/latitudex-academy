428b06622246ea02b02a2441ca7eeea372fd0bd7
lc-d <lorenzo@cataldi.design>
2023-11-03 11:13:41 +0100

    add image dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@nuxtjs/robots']
-
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@nuxtjs/robots'],
+  image: {
+    dir: 'assets'
+  }
 })
