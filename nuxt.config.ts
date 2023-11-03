242328186a7fbf03ef8df5efba3fa72b0dab7a45
lc-d <lorenzo@cataldi.design>
2023-11-03 12:40:53 +0100

    add basepath

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@nuxtjs/robots']
+  modules: ['@nuxtjs/tailwindcss', "@nuxt/image", '@nuxtjs/robots'],
+  app: {
+    baseURL: '/latitudex-academy'
+  }
 })
