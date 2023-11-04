2aea9087229f510ca60de0b956deae923ad8464a
lc-d <lorenzo@cataldi.design>
2023-11-04 14:06:02 +0100

    fixup! updates

--- components/AppSectionTeachers.vue
+++ components/AppSectionTeachers.vue
@@ -7,12 +7,12 @@
         </h2>
       </div>
       <div class="flex justify-center mt-8">
-        <ul class="md:flex md:space-x-20 justify-center">
-          <li class="prose w-64 flex flex-col items-center mb-8">
-            <figure class="img-spacer ratio-1x1 rounded-full w-64">
-              <BaseImage :width="420" :src="''" :format="'webp'" />
+        <ul class="md:flex md:space-x-12 justify-center">
+          <li class="prose w-52 flex flex-col items-center mb-8">
+            <figure class="img-spacer ratio-1x1 rounded-full w-56">
+              <BaseImage :width="220" :src="''" :format="'webp'" sizes="100vw sm:50vw md:220" />
             </figure>
-            <h3 class="!mt-0">Giulia Gabani</h3>
+            <h3 class="!mt-0 !text-base">Giulia Gabani</h3>
             <ul class="text-sm pl-0">
               <li>lorem ipsum</li>
               <li>lorem ipsum</li>
@@ -20,11 +20,11 @@
               <li>lorem ipsum</li>
             </ul>
           </li>
-          <li class="prose w-64 flex flex-col items-center mb-8">
-             <figure class="img-spacer ratio-1x1 rounded-full w-64">
-              <BaseImage :width="420" :src="''" :format="'webp'" />
+          <li class="prose w-52 flex flex-col items-center mb-8">
+             <figure class="img-spacer ratio-1x1 rounded-full w-56">
+              <BaseImage :width="220" :src="''" :format="'webp'" sizes="100vw sm:50vw md:220" />
             </figure>
-            <h3 class="!mt-0">Francesco Maria Sauro</h3>
+            <h3 class="!mt-0 !text-base">Francesco Maria Sauro</h3>
             <ul class="text-sm pl-0">
               <li>lorem ipsum</li>
               <li>lorem ipsum</li>
@@ -32,11 +32,11 @@
               <li>lorem ipsum</li>
             </ul>
           </li>
-          <li class="prose w-64 flex flex-col items-center mb-8">
-             <figure class="img-spacer ratio-1x1 rounded-full w-64">
-              <BaseImage :width="420" :src="''" :format="'webp'" />
+          <li class="prose w-52 flex flex-col items-center mb-8">
+             <figure class="img-spacer ratio-1x1 rounded-full w-56">
+              <BaseImage :width="220" :src="''" :format="'webp'" sizes="100vw sm:50vw md:220" />
             </figure>
-            <h3 class="!mt-0">Luca Carenzo</h3>
+            <h3 class="!mt-0 !text-base">Luca Carenzo</h3>
             <ul class="text-sm pl-0">
               <li>lorem ipsum</li>
               <li>lorem ipsum</li>
