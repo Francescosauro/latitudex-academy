d959268b3fbc2195983244ca94878a161bd6e30c
lc-d <lorenzo@cataldi.design>
2023-11-04 14:08:53 +0100

    fixup! updates

--- components/AppSectionVideo.vue
+++ components/AppSectionVideo.vue
@@ -2,19 +2,19 @@
   <section class="py-10">
     <div class="wrapper">
       <div class="md:flex md:space-x-4">
-        <div class="md:w-2/3 mb-5 relative">
+        <div class="md:w-2/3 mb-5 relative overflow-hidden">
           <button
             v-if="isPaused"
             @click="play()"
             type="button"
             aria-controls="video-player"
             aria-label="Play"
-            class="absolute right-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-full"
+            class="img-spacer absolute right-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-full"
           >
             <BaseImage
               src="play-btn.jpg"
               alt="Play"
-              class="m-auto"
+              class="m-auto ratio-16x9"
               width="1318"
               height="748"
               sizes="100vw sm:50vw md:1318"
