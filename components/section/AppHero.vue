dc788dd7ec119d822df784369ae3380ab679660f
lc-d <lorenzo@cataldi.design>
2023-12-18 09:05:29 +0100

    fixup! text update

--- components/section/AppHero.vue
+++ components/section/AppHero.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="max-w-[40rem] prose m-auto py-16 text-center">
-    <h1 class="mb-3 text-gradient-primary">L'accademia di Primo Soccorso</h1>
+    <h1 class="mb-3 text-gradient-primary">L'Accademia di Primo Soccorso</h1>
     <h2 class="mt-0 font-normal text-gradient-primary">
       Unisciti alla prima scuola in Italia di Primo Soccorso. Per tutti, online e in
       presenza.
