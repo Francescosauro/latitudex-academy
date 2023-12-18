4c154bfe3e7ccec6ae71aa3b3bb9e88507cc7933
lc-d <lorenzo@cataldi.design>
2023-12-18 09:02:14 +0100

    text update

--- components/section/AppHero.vue
+++ components/section/AppHero.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="max-w-[40rem] prose m-auto py-16 text-center">
-    <h1 class="mb-3 text-gradient-primary">La scuola di primo soccorso</h1>
+    <h1 class="mb-3 text-gradient-primary">L'accademia di Primo Soccorso</h1>
     <h2 class="mt-0 font-normal text-gradient-primary">
-      Unisciti alla prima scuola in Italia di primo soccorso. Per tutti, online e in
+      Unisciti alla prima scuola in Italia di Primo Soccorso. Per tutti, online e in
       presenza.
     </h2>
     <BaseLink :to="{ path: '/', hash: '#corsi-online' }" class="btn btn-lg !no-underline">
