1b82c550b178f0d310b514fd3d340a7c38b33705
lc-d <lorenzo@cataldi.design>
2024-02-02 23:00:29 +0100

    setup landing components

---
+++ components/landing/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="!mb-2 uppercase">Contenuto dei corsi</p>
+  <h2 class="mt-0">
+    Corso di primo soccorso
+  </h2>
+  <p>
+    Un programma completo di lezioni teoriche e dimostrazioni pratiche simulate su manichini ad alta fedeltà per insegnarti ad affrontare tutte le più comuni situazioni di emergenza. Potrai rivedere le lezioni quante volte vorrai, e acquisire totale fiducia nelle manovre da eseguire. 
+  </p>
+  <p>XXX sezioni - XXX lezioni - durata totale: XXXh XXXm</p>
+
+
+
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
---
+++ components/landing/How.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2 class="mt-0">
+    Come funziona
+  </h2>
+  <ol>
+    <li>Acquisiti il corso e accedi alla piattaforma con XXX</li>
+    <li>Guardi i video quando puoi, scegli tu i tempi</li>
+    <li>Superi il test finale di valutazione</li>
+    <li>(Solo corso BLS-D) Vieni alla prova finale e ottieni il certificato ufficiale</li>
+  </ol>
+  <BaseImage
+    :src="'mockup-video.png'"
+    alt=""
+    class="mb-0 w-full"
+    width="571"
+    height="401"
+    format="webp"
+    sizes="100vw sm:50vw md:571" />
+  <p>
+    Tutti i video si trovano su un’unica piattaforma semplice e intuitva da usare, grazie a XXX.
+  </p>
+  <div class="text-center">
+    <BaseLink class="btn btn-lg !no-underline" :is-link="true" :to="{ path: '/corso-primo-soccorso', hash: '#acquista' }">
+      <strong>Acquista</strong>
+    </BaseLink>
+  </div>
+</template>
