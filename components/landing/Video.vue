0ca1446f90a6a173378093a77afe05e5ecd1b78e
lc-d <lorenzo@cataldi.design>
2024-03-10 15:10:11 +0100

    add video controller

--- components/landing/Video.vue
+++ components/landing/Video.vue
@@ -3,29 +3,37 @@
     <div class="wrapper">
       <div class="md:flex md:space-x-4">
         <div class="md:w-2/3 mb-5 relative overflow-hidden">
-          <button
-            v-if="isPaused"
-            @click="play()"
-            type="button"
-            aria-controls="video-player"
-            aria-label="Play"
-            class="z-[1] img-spacer absolute right-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full h-full"
-          >
-            <BaseImage
-              src="play-btn.jpg"
-              alt="Play"
-              class="m-auto ratio-16x9"
-              width="1318"
-              height="748"
-              format="webp"
-              sizes="100vw sm:50vw md:1318"
-              :lazy="false"
-            />
-          </button>
-          <video id="video-player" ref="videoPlayer" width="1280" height="720" controls>
-            <source src="/primo-soccorso-spot.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <nav v-if="playedByUser" aria-label="Controlli video">
+            <button
+              @click="toggleMuted()"
+              class="bottom-2 right-14 absolute text-secondary z-10 bg-white rounded-full p-2 shadow hover:shadow-lg"
+              :aria-label="isMuted ? 'Attiva l\'audio del video' : 'Metti in muto'"
+              :title="isMuted ? 'Attiva l\'audio del video' : 'Metti in muto'">
+              <BaseIcon v-if="isMuted" :icon="'mute'" :size="''" />
+              <BaseIcon v-else :icon="'unmute'" :size="''" />
+            </button>
+            <button
+              @click="togglePlay()"
+              class="bottom-2 left-2 absolute text-secondary z-10 bg-white rounded-full p-2 shadow hover:shadow-lg"
+              :aria-label="isPlayed ? 'Ferma il video' : 'Fai partire il video'"
+              :title="isPlayed ? 'Ferma il video' : 'Fai partire il video'">
+              <BaseIcon v-if="isPlayed" :icon="'pause'" :size="''" />
+              <BaseIcon v-else :icon="'play'" :size="''" />
+            </button>
+            <button
+              @click="toggleFullscreen()"
+              class="bottom-2 right-2 absolute text-secondary z-10 bg-white rounded-full p-2 shadow hover:shadow-lg"
+              aria-label="Metti il video a tutto schermo"
+              title="Metti il video a tutto schermo">
+              <BaseIcon :icon="'fullscreen'" :size="''" />
+            </button>
+          </nav>
+          <div @click="togglePlay(); playedByUser = true">
+            <video id="video-player" ref="videoPlayer" width="1280" height="720" poster="/video-poster.jpg">
+              <source src="/primo-soccorso-spot.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
         <header class="md:w-1/3 prose">
           <h2>
@@ -47,10 +55,29 @@
 </template>
 
 <script setup lang="ts">
-const videoPlayer: Ref = ref(null);
-let isPaused = ref(true);
-const play = () => {
-  isPaused.value = false;
-  if(videoPlayer) videoPlayer.value.play();
+const videoPlayer: Ref<HTMLVideoElement | null> = ref(null)
+const playedByUser: Ref<boolean> = ref(false)
+const isMuted: Ref<boolean> = ref(false)
+const isPlayed: Ref<boolean> = ref(false)
+
+const toggleFullscreen = () => {
+  if (!videoPlayer.value) return
+  videoPlayer.value.requestFullscreen()
+}
+const togglePlay = () => {
+  if (!videoPlayer.value) return
+  (videoPlayer.value.paused) ?
+    videoPlayer.value.play() :
+    videoPlayer.value.pause()
+  isPlayed.value = !isPlayed.value
+}
+
+const toggleMuted = () => {
+  if (!videoPlayer.value) return
+  (videoPlayer.value && videoPlayer.value.muted) ?
+    videoPlayer.value.muted = false :
+    videoPlayer.value.muted = true
+  isMuted.value = !isMuted.value
 };
+
 </script>
